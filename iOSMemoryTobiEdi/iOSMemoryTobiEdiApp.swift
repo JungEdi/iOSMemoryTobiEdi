640e4b36ce1cc105993281017dd82e70dc06f4cf
Tobias Wyss <towyss@gmail.com>
2022-10-13 16:17:02 +0200

    bootstrap app

---
+++ iOSMemoryTobiEdi/iOSMemoryTobiEdiApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSMemoryTobiEdiApp.swift
+//  iOSMemoryTobiEdi
+//
+//  Created by Tobi Wyss on 13.10.22.
+//
+
+import SwiftUI
+
+@main
+struct iOSMemoryTobiEdiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
